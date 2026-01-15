fe60eee558813af1b38346a6310186b05b7bfea2
Jaohenri <joaohrcf10@gmail.com>
2026-01-14 23:26:21 -0300

    Finalizing file_log implementation and documenting the two decorators

---
+++ file_log/file_log.py
@@ -0,0 +1,31 @@
+"""Decorator to register functions execution in a log"""
+
+import time
+def file_log(func):
+    """Registers the execution of a funcion in a log file"""
+    def wrapper(*args, **kwargs):
+        
+        with open("file_log/log.txt", "a") as file:
+            func(*args, **kwargs)
+            times = time.strftime("%d-%m-%Y %H:%M:%S")
+            file.write(f"[{times}] The function <{func.__name__}> was executed.\n")
+    return wrapper
+
+@file_log
+def send_email(recipient: str) -> None:
+    """Simulates sending an email to the specified recipient."""
+
+    print(f"Sending email to {recipient}...")
+
+
+@file_log
+def calculate_salary(worked_hours: int, hourly_rate: float) -> None:
+    """Calculates an employee's salary based on worked hours and hourly rate."""
+
+    print(f"The employee's salary is {worked_hours * hourly_rate}.")
+
+
+if __name__ == "__main__":
+    send_email("joao@example.com")
+    calculate_salary(200, 30.5)
+    send_email("joao@example.com")
--- func_time/func_time.py
+++ func_time/func_time.py
@@ -1,9 +1,16 @@
+"""Decorator to measure functions executing time"""
+
 import time
 
 def func_time(func):
-    def wrapper(text):
+    """Measures and prints the execution time of a function.
+
+    This decorator wraps a function, records the time immediately before
+    and after its execution, and prints the elapsed time in seconds.
+    """
+    def wrapper(*args, **kwargs):
         start = time.time()
-        func(text)
+        func(*args, **kwargs)
         end = time.time()
         elapsed = end - start
         print(f"A function {func.__name__} executed in {elapsed:.4f} seconds")
